e257622569dba891d90838c58ebd116fcf8c9d3a
mhand <mhandmaous@gmail.com>
2026-08-11 19:57:35 +0100

    fix(fe): build dist

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  outputDir: "build",
   define: {
     "import.meta.env.VITE_API_URL": JSON.stringify('__VITE_API_URL__')
   }
